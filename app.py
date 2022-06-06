d8880d39b45b1cea16634fe34604830f3c2876b3
SuyashChopda <73349399+SuyashChopda@users.noreply.github.com>
2022-06-06 10:45:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>
-            <p> This is demo app</p>"
+    return "<p>Hello, World!, hi this is demo</p>"
     
